1751b5cb463360e7f00ec06bae873acbe76db454
gabrielGC <gabriel225gc@gmail.com>
2024-09-04 15:09:53 -0300

    Permissão para acesso com frontend

--- src/main/java/com/addressapi/adressapi/controller/EnderecoController.java
+++ src/main/java/com/addressapi/adressapi/controller/EnderecoController.java
@@ -3,13 +3,11 @@ package com.addressapi.adressapi.controller;
 import com.addressapi.adressapi.model.Endereco;
 import com.addressapi.adressapi.service.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/enderecos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EnderecoController {
     @Autowired
     private ViaCepService viaCepService;
